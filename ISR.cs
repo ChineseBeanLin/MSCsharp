a2b88042b6ee34155b9e70d74ff38fa5ac29f64a
LinHan <lhfrom1905@outlook.com>
2019-04-18 21:20:59 +0800

    TTS,MSC,ISR
    不保证不出问题，有一些格式问题，像是返回值为char*但接收为string，在ISR中有修改为IntPtr，其他的没有修改。

---
+++ ISR.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace NCHUVA.MSC.ISR
+{
+    #region ISR enum
+    public enum AudioStatus
+    {
+
+        MSP_AUDIO_SAMPLE_FIRST = 1, //第一块音频
+        MSP_AUDIO_SAMPLE_CONTINUE = 2, //还有后继音频
+        MSP_AUDIO_SAMPLE_LAST = 4 //最后一块音频
+
+    }
+
+    public enum EpStatus
+    {
+        MSP_EP_LOOKING_FOR_SPEECH = 0, //还没有检测到音频的前端点。
+        MSP_EP_IN_SPEECH = 1, //已经检测到了音频前端点，正在进行正常的音频处理。
+        MSP_EP_AFTER_SPEECH = 3, //检测到音频的后端点，后继的音频会被MSC忽略。
+        MSP_EP_TIMEOUT = 4, //超时。
+        MSP_EP_ERROR = 5, //    出现错误。
+        MSP_EP_MAX_SPEECH = 6 //  音频过大。
+
+    }
+
+    public enum RsltStatus
+    {
+        MSP_REC_STATUS_SUCCESS = 0, //识别成功，此时用户可以调用QISRGetResult来获取（部分）结果。
+        MSP_REC_STATUS_NO_MATCH = 1, //识别结束，没有识别结果。
+        MSP_REC_STATUS_INCOMPLETE = 2, //正在识别中。
+        MSP_REC_STATUS_COMPLETE = 5 //识别结束。
+    }
+    #endregion 
+    public class ISRDLL
+    {
+        [DllImport("msc.dll", CallingConvention = CallingConvention.Winapi)]
+        public static extern IntPtr QISRSessionBegin(string grammarList, string _params, ref int errorCode);
+        [DllImport("msc.dll", CallingConvention = CallingConvention.Winapi)]
+        public static extern int QISRAudioWrite(string sessionID, byte[] waveData, uint waveLen, int audioStatus, ref int epStatus, ref int recogStatus);
+        [DllImport("msc.dll", CallingConvention = CallingConvention.Winapi)]
+        public static extern IntPtr QISRGetResult(string sessionID, ref int rsltStatus, int waitTime, ref int errorCode);
+        [DllImport("msc.dll", CallingConvention = CallingConvention.Winapi)]
+        public static extern int QISRSessionEnd(string sessionID, string hints);
+        [DllImport("msc.dll", CallingConvention = CallingConvention.Winapi)]
+        public static extern int QISRGetParam(string sessionID, string paramName, string paramValue, ref uint valueLen);
+    }
+}
---
+++ TTS.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace NCHUVA.MSC.TTS
+{
+
+    #region TTS枚举常量
+    /// <summary>
+    /// vol参数的枚举常量
+    /// </summary>
+    public enum EnuVol
+    {
+        x_soft,
+        soft,
+        medium,
+        loud,
+        x_loud
+    }
+
+    /// <summary>
+    /// speed语速参数的枚举常量
+    /// </summary>
+    public enum EnuSpeed
+    {
+        x_slow,
+        slow,
+        medium,
+        fast,
+        x_fast
+    }
+    /// <summary>
+    /// speeker朗读者枚举常量
+    /// </summary>
+    public enum EnuSpeeker
+    {
+        小燕_青年女声_中英文_普通话 = 0,
+        小宇_青年男声_中英文_普通话,
+        凯瑟琳_青年女声_英语,
+        亨利_青年男声_英语,
+        玛丽_青年女声_英语,
+        小研_青年女声_中英文_普通话,
+        小琪_青年女声_中英文_普通话,
+        小峰_青年男声_中英文_普通话,
+        小梅_青年女声_中英文_粤语,
+        小莉_青年女声_中英文_台普,
+        小蓉_青年女声_汉语_四川话,
+        小芸_青年女声_汉语_东北话,
+        小坤_青年男声_汉语_河南话,
+        小强_青年男声_汉语_湖南话,
+        小莹_青年女声_汉语_陕西话,
+        小新_童年男声_汉语_普通话,
+        楠楠_童年女声_汉语_普通话,
+        老孙_老年男声_汉语_普通话
+    }
+
+    public enum SynthStatus
+    {
+        MSP_TTS_FLAG_STILL_HAVE_DATA = 1,
+        MSP_TTS_FLAG_DATA_END = 2,
+        MSP_TTS_FLAG_CMD_CANCELED = 0
+    }
+    #endregion
+    public class TTSDll
+    {
+
+        #region TTS dll import
+
+        [DllImport("msc.dll", CallingConvention = CallingConvention.Winapi)]
+        public static extern IntPtr QTTSSessionBegin(string _params, ref int errorCode);
+
+        [DllImport("msc.dll", CallingConvention = CallingConvention.Winapi)]
+        public static extern int QTTSTextPut(string sessionID, string textString, uint textLen, string _params);
+
+        [DllImport("msc.dll", CallingConvention = CallingConvention.Winapi)]
+        public static extern IntPtr QTTSAudioGet(string sessionID, ref uint audioLen, ref SynthStatus synthStatus, ref int errorCode);
+
+        [DllImport("msc.dll", CallingConvention = CallingConvention.Winapi)]
+        public static extern int QTTSGetParam(string sessionID, string paramName, string paramValue, ref uint valueLen);
+
+        [DllImport("msc.dll", CallingConvention = CallingConvention.Winapi)]
+        public static extern int QTTSSessionEnd(string sessionID, string hints);
+        #endregion
+    }
+}
